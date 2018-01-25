8cafcfca96d9ed3a2d4180ccb6118a8b42d4ee2f
javidan salarian <javidansalarian@javidans-mbp.hitronhub.home>
2018-01-24 23:16:34 -0800

    User can add new items to the list. Create an alert view and addNewItem button.

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemList = ["a","b","c"]
-    var selectedNum: Int?
+    var itemList = ["a","b","c"]
+    var alertTextField: UITextField?
     
 
     override func viewDidLoad() {
@@ -32,64 +32,44 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
         let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
-       // cell.he= 100.0
-        let multiplier: Float = Float(255/itemList.count) - Float(itemList.count + indexPath.row)
-        print(multiplier)
         let cellColor = UIColor.brown
         cell.backgroundColor = cellColor
-            // UIColor(red: 82.0, green: 82.0, blue: 88.0, alpha: 1.0)
         cell.textLabel?.text = itemList[indexPath.row]
         return cell
-        
-        
-        
+ 
     }
     
     //MARK: TableView Delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemList[indexPath.row])
-     //   print(tableView.cellForRow(at: indexPath)?.accessoryType )
 
-        
         if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
       tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-
         }
-
-//
-//        if selectedNum == nil {
-//            selectedNum = indexPath.row
-//            print("1")
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//
-//
-//        } else {
-//           // tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//
-//            if selectedNum == indexPath.row {
-//            selectedNum = nil
-//            print("2")
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//
-//        } else {
-//            selectedNum = indexPath.row
-//            print("1")
-//
-//            }
-//        }
-//
-        
     }
-    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        
-    }
-    
-    
-    
-    
 
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+            
+            // What will happen once the user clicks the Add Item button on our UIAlert
+            if let newItemText = self.alertTextField!.text,
+                self.alertTextField?.text != ""  {
+                
+                self.itemList.append(newItemText)
+                self.tableView.reloadData()
+            }
+        }
+        alert.addAction(action)
+        alert.addTextField { textField in
+            self.alertTextField = textField
+            textField.placeholder = "Create new item"
+        }
+        present(alert, animated: true, completion: nil)
+    }
 }
 
